ca6cc0161060aeaf349d6b7f62f094b47f1b6083
felipe <feot1000@gmail.com>
2025-09-13 21:52:29 +0200

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import { Toaster } from "sonner"
+
 import "./globals.css"
 
 export const metadata: Metadata = {
